ff88ae3b0aacef6ac3a4e3f8794bf46c00ace83c
sirEpok <53379114+sirEpok@users.noreply.github.com>
2021-02-17 10:23:11 +0500

    14Lesson. Edit №6

--- config/init_db.sql
+++ config/init_db.sql
@@ -16,6 +16,7 @@ CREATE TABLE public.contact
     resume_uuid character(36) COLLATE pg_catalog."default" NOT NULL,
     type text COLLATE pg_catalog."default" NOT NULL,
     value text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT contact_pkey PRIMARY KEY (id),
     CONSTRAINT contact_resume_uuid_fkey FOREIGN KEY (resume_uuid)
         REFERENCES public.resume (uuid) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -31,3 +32,21 @@ CREATE UNIQUE INDEX contact_uuid_type_index
     ON public.contact USING btree
         (resume_uuid COLLATE pg_catalog."default" ASC NULLS LAST, type COLLATE pg_catalog."default" ASC NULLS LAST)
     TABLESPACE pg_default;
+
+CREATE TABLE public.section
+(
+    id SERIAL NOT NULL,
+    resume_uuid character(36) COLLATE pg_catalog."default" NOT NULL,
+    type text COLLATE pg_catalog."default" NOT NULL,
+    content text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT section_pkey PRIMARY KEY (id),
+    CONSTRAINT section_resume_uuid_fkey FOREIGN KEY (resume_uuid)
+        REFERENCES public.resume (uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE public.section
+    OWNER to postgres;
--- config/populate.sql
+++ config/populate.sql
@@ -1,4 +1,4 @@
 INSERT INTO resume (uuid, full_name) VALUES
   ('123', 'Full Name1'),
-  ('123', 'Full Name2'),
-  ('123', 'Full Name3');
+  ('124', 'Full Name2'),
+  ('125', 'Full Name3');
