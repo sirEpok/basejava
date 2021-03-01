24e1e7bebfcf06b67d2d336ab30db5c027c83c53
sirEpok <53379114+sirEpok@users.noreply.github.com>
2021-03-01 14:38:25 +0500

    16Lesson. Edit №2

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -55,6 +55,10 @@ public class Resume implements Comparable<Resume>, Serializable {
         return uuid;
     }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    
     public String getFullName() {
         return fullName;
     }
