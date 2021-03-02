fae387156807cdd3f1cad9e382e5762533861f8e
sirEpok <53379114+sirEpok@users.noreply.github.com>
2021-03-02 14:33:47 +0500

    16Lesson. Edit №3

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,52 +1,22 @@
-CREATE TABLE public.resume
-(
-    uuid character(36) COLLATE pg_catalog."default" NOT NULL,
-    full_name text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT resume_pkey PRIMARY KEY (uuid)
-)
+CREATE TABLE resume (
+                        uuid      CHAR(36) PRIMARY KEY NOT NULL,
+                        full_name TEXT                 NOT NULL
+);
 
-    TABLESPACE pg_default;
-
-ALTER TABLE public.resume
-    OWNER to postgres;
-
-CREATE TABLE public.contact
-(
-    id SERIAL NOT NULL,
-    resume_uuid character(36) COLLATE pg_catalog."default" NOT NULL,
-    type text COLLATE pg_catalog."default" NOT NULL,
-    value text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT contact_pkey PRIMARY KEY (id),
-    CONSTRAINT contact_resume_uuid_fkey FOREIGN KEY (resume_uuid)
-        REFERENCES public.resume (uuid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-)
-
-    TABLESPACE pg_default;
-
-ALTER TABLE public.contact
-    OWNER to postgres;
-
+CREATE TABLE contact (
+                         id          SERIAL,
+                         resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+                         type        TEXT     NOT NULL,
+                         value       TEXT     NOT NULL
+);
 CREATE UNIQUE INDEX contact_uuid_type_index
-    ON public.contact USING btree
-        (resume_uuid COLLATE pg_catalog."default" ASC NULLS LAST, type COLLATE pg_catalog."default" ASC NULLS LAST)
-    TABLESPACE pg_default;
+  ON contact (resume_uuid, type);
 
-CREATE TABLE public.section
-(
-    id SERIAL NOT NULL,
-    resume_uuid character(36) COLLATE pg_catalog."default" NOT NULL,
-    type text COLLATE pg_catalog."default" NOT NULL,
-    content text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT section_pkey PRIMARY KEY (id),
-    CONSTRAINT section_resume_uuid_fkey FOREIGN KEY (resume_uuid)
-        REFERENCES public.resume (uuid) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-    TABLESPACE pg_default;
-
-ALTER TABLE public.section
-    OWNER to postgres;
+CREATE TABLE section (
+                         id          SERIAL PRIMARY KEY,
+                         resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+                         type        TEXT     NOT NULL,
+                         content     TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX section_idx
+  ON section (resume_uuid, type);
