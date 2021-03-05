33fc60e2a4dd15bed8457ee9a7b6b45e8b87e87f
sirEpok <53379114+sirEpok@users.noreply.github.com>
2021-03-05 10:28:22 +0500

    17Lesson. Edit №1

--- config/populate.sql
+++ config/populate.sql
@@ -1,4 +1,4 @@
 INSERT INTO resume (uuid, full_name) VALUES
-  ('123', 'Full Name1'),
-  ('124', 'Full Name2'),
-  ('125', 'Full Name3');
+('123', 'Full Name1'),
+('124', 'Full Name2'),
+('125', 'Full Name3');
